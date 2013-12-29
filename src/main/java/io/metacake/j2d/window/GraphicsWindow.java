d70721fa99c480ebb865b9d5b22e9c9aba622fcf
Ryan Plessner <plessner.ryan@gmail.com>
2013-12-28 20:28:45 -0500

    added some documentation to the graphics window

--- src/main/java/io/metacake/j2d/window/GraphicsWindow.java
+++ src/main/java/io/metacake/j2d/window/GraphicsWindow.java
@@ -7,6 +7,16 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+/**
+ * The {@code GraphicsWindow} is a {@link io.metacake.core.common.window.CakeWindow} that uses a {@link javax.swing.JFrame}
+ * as its raw window.
+ * <p>
+ *      There are several notable points about hte {@code GraphicsWindow}'s implementation.
+ *      Firstly, it uses a {@link java.awt.event.WindowAdapter} to call {@link io.metacake.j2d.window.GraphicsWindow#close()}.
+ *      It is called during the {@link java.awt.event.WindowAdapter#windowClosing(java.awt.event.WindowEvent)} event callback.
+ *      Second, it's size is not actually the width and height that is specified in the constructor. The {@link javax.swing.JFrame}'s
+ *      actual size includes the size of the insets on all sides, so that drawing area is the size of the given width and height.
+ */
 public class GraphicsWindow extends CakeWindow<JFrame> {
     private JFrame frame = new JFrame();
 
