6c20d1a92cb6c3abbb50a9f1bee31f31dbe82726
Ryan Plessner <plessner.ryan@gmail.com>
2013-12-31 14:23:49 -0500

    moved the constructor to the top

--- src/main/java/io/metacake/j2d/process/keyrecognizers/KeyReleaseRecognizer.java
+++ src/main/java/io/metacake/j2d/process/keyrecognizers/KeyReleaseRecognizer.java
@@ -4,15 +4,16 @@ import io.metacake.core.process.ActionRecognizerName;
 import io.metacake.j2d.process.recognizers.ReleaseActionRecognizer;
 
 public class KeyReleaseRecognizer extends ReleaseActionRecognizer implements KeyActionRecognizer {
+
+    public KeyReleaseRecognizer(ActionRecognizerName name) {
+        super(name);
+    }
+
     @Override
     public void keyPressed(long timestamp) {}
 
     @Override
     public void keyReleased(long timestamp) {
         setTriggered(true);
-    }
-
-    public KeyReleaseRecognizer(ActionRecognizerName name) {
-        super(name);
     }
 }
